de5b985be06f18ef651ac1b04ba7bdfa2b3903c3
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-08-13 12:33:01 +0300

    Updates

--- static/js/FilmsView.62d695c7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-05.1-movies"]=this["webpackJsonpgoit-react-hw-05.1-movies"]||[]).push([[2],{89:function(t,e,i){"use strict";i.r(e);var s=i(3);e.default=function(){return Object(s.jsx)("h1",{children:"Films"})}}}]);
-//# sourceMappingURL=FilmsView.62d695c7.chunk.js.map
